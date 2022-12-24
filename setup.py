8c937804403b79613c6744ea666893e290bffec2
evanhongo <evanhongo@gmail.com>
2022-12-24 20:19:26 +0800

    fix(setup.py): version 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="cz_github_convention",
-    version="0.1.0",
+    version="0.1.2",
     py_modules=["cz_github_convention"],
     author="evanhongo",
     author_email="evanhongo@gmail.com",
